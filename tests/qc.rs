cce58a810bc5c7fb0549ff6226778111ba187dec
Ivan Boldyrev <lispnik@gmail.com>
2025-08-13 04:14:29 +0200

    Add a quickcheck test

--- tests/qc.rs
+++ tests/qc.rs
@@ -3,16 +3,14 @@ use ring_lwe::{
 };
 
 #[quickcheck_macros::quickcheck]
-pub fn qc_test_basic(mut message: String) {
-    if message.contains('\0') {
-        message = message.replace('\0', " "); // Remove null characters
-    }
+pub fn qc_test_basic(mut message: Vec<u8>) {
+    message.push(1);
     let seed = None;
     let params = Parameters::default();
     let keypair = keygen_bytes(&params, seed);
     let pk = keypair.public;
     let sk = keypair.secret;
-    let ciphertext = encrypt_bytes(&pk, &message.as_bytes(), &params, seed);
+    let ciphertext = encrypt_bytes(&pk, &message, &params, seed);
     let decrypted_message = decrypt_bytes(&sk, &ciphertext, &params);
-    assert_eq!(message.as_bytes(), decrypted_message, "test failed");
+    assert_eq!(message, decrypted_message, "test failed");
 }
